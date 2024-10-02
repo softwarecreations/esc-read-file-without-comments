968a15a75443f2167ebf9fd020097a6b910683b1
Joshua Maserow <joshua@softwarecreations.co.za>
2024-10-02 15:17:54 +0200

    'Added required fs import'

--- main.mjs
+++ main.mjs
@@ -1,5 +1,7 @@
 'use strict';
 
+import fs from 'fs';
+
 export const readFileWithoutComments = filePath => fs.readFileSync(filePath,'utf8').replace(/^\s*#.*$/gm,'\n');
 
 export default readFileWithoutComments;
